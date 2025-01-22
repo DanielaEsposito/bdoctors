d4332cd268d64371b5ca8363b662ebb826465908
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-01-22 12:07:13 +0100

    add index

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,12 +1,26 @@
 const connection=require("../db/connectionDb");
 //index
 function index (req,res){
+    const sql= 'SELECT * FROM doctors'
+    connection.query(sql,(err, results)=>{
+        if (err)return res.status(500).json ({
+            error:'Database query failed'
+            
+        })
+      
+        res.json({
+            status: "ok",
+            results
+        });
+        console.log(res);
+    })
 
 };
 //show
 function show (req,res){
+    
+}
 
-};
 //create
 function create (req,res){
 
