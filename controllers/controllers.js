88016ab25601ad4a2f6d430fed8e5e282fb7bb12
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-01-22 12:28:33 +0100

    add show

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -19,8 +19,75 @@ function index (req,res){
 };
 //show
 function show (req,res){
-    
+    const id = parseInt(req.params.id);
+    const sqlDoctors ="SELECT * FROM `doctors` WHERE `id` = ? ";
+    connection.query(sqlDoctors,[id],(err, doctorsResults)=>{
+        if(err){
+           console.log(err);
+           return res.tatus(500).json({
+           error: "Database query failed"})  ;     
+        }
+        if(doctorsResults.lenght === 0){
+           return res.status(404).json({error: "doctor not found"});
+        }
+        const doctor = doctorsResults[0];
+
+     
+        const sqlReviews = `
+            SELECT reviews.*
+            FROM reviews
+            INNER JOIN doctors
+            ON doctors.id = reviews.doctor_id
+            WHERE doctors.id = ?`;
+        connection.query(sqlReviews, [id], (err, reviewResults) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    error: "Database query failed"
+                });
+            }
+            const reviews= reviewResults
+
+            const sqlSpecialty = `
+            SELECT specialties.*
+            FROM specialties
+            INNER JOIN doctors
+            ON doctors.specialty_id = specialties.id
+            WHERE doctors.id = ?`;
+            connection.query(sqlSpecialty, [id], (err, specialtyResults) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    error: "Database query failed"
+                });
+            }
+            res.json({
+                status: "ok",
+                doctor: {
+                    ...doctor,
+                    ...reviews,
+                    specialty:specialtyResults
+                }
+            });
+            
+            })
+
+           
+
+        });
+        
+        
+        
+       })
+   
 }
+// res.json({
+//     status: "ok",
+//     doctor: {
+//         ...doctor,
+//         reviews:reviewResults
+//     }
+// });
 
 //create
 function create (req,res){
