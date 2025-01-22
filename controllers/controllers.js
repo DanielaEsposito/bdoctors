372c130287fc47619fa20708c2626c5b9cee966a
antonioferrigno28 <ferrigno1@hotmail.it>
2025-01-22 11:57:25 +0100

    store-working

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,26 +1,75 @@
-const connection=require("../db/connectionDb");
+const connection = require("../db/connectionDb");
 //index
-function index (req,res){
-
-};
+function index(req, res) {}
 //show
-function show (req,res){
-
-};
+function show(req, res) {}
 //create
-function create (req,res){
+function storeDoctor(req, res) {
+  const {
+    name,
+    surname,
+    specialty_id,
+    email,
+    phone_number,
+    address,
+    description,
+    city,
+    province,
+  } = req.body;
 
-};
+  if (
+    !email ||
+    !name ||
+    !surname ||
+    !phone_number ||
+    !address ||
+    !specialty_id
+  ) {
+    return res.status(400).json({
+      status: "ko",
+      message:
+        "Manca uno dei campi obbligatori: Email, Name, Surname, Phone, Address, or Specialization",
+    });
+  }
+
+  const sql = `
+        INSERT INTO doctors (name, surname, specialty_id, email, phone_number, address, description, city, province)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+      `;
+
+  connection.query(
+    sql,
+    [
+      name,
+      surname,
+      specialty_id,
+      email,
+      phone_number,
+      address,
+      description || null,
+      city || null,
+      province || null,
+    ],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({
+          status: "ko",
+          message: "Database query failed",
+        });
+      }
+
+      res.json({
+        status: "ok",
+        message: "Dottore registrato!",
+      });
+    }
+  );
+}
 //modify
-function modify (req,res){
-
-};
+function modify(req, res) {}
 //update
-function update (req,res){
-
-};
+function update(req, res) {}
 //destroy
-function destroy (req,res){
-
-};
-module.exports ={index, show, create, modify, update, destroy};
+function destroy(req, res) {}
+module.exports = { index, show, storeDoctor, modify, update, destroy };
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Controller = require("../controllers/controllers")
+const Controller = require("../controllers/controllers");
 //! LISTA ROTTE
 
 // index
-router.get('/',Controller.index);
+router.get("/", Controller.index);
 // Show
-router.get ("/:id", Controller.show);
+router.get("/:id", Controller.show);
 //Create
-router.post ("/", Controller.create);
+router.post("/", Controller.storeDoctor);
 //Update
-router.put ("/:id",Controller.update );
+router.put("/:id", Controller.update);
 //Modify
-router.patch ("/:id", Controller.modify);
+router.patch("/:id", Controller.modify);
 //Delete
-router.delete ("/:id", Controller.destroy);
+router.delete("/:id", Controller.destroy);
 
-module.exports= router
+module.exports = router;
