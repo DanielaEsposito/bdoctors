905ab04ebcd042b8be5f1a0b182db3c464535e2c
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-01-22 11:42:04 +0100

    setup

---
+++ routes/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const Controller = require("../controllers/controllers")
+//! LISTA ROTTE
+
+// index
+router.get('/',Controller.index);
+// Show
+router.get ("/:id", Controller.show);
+//Create
+router.post ("/", Controller.create);
+//Update
+router.put ("/:id",Controller.update );
+//Modify
+router.patch ("/:id", Controller.modify);
+//Delete
+router.delete ("/:id", Controller.destroy);
+
+module.exports= router
